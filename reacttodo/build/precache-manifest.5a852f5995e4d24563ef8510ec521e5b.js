26561337231ced24dbd2a2059f99d012627add39
Donovan <DonBond@users.noreply.github.com>
2018-12-02 20:50:01 +0200

    Add files via upload

---
+++ reacttodo/build/precache-manifest.5a852f5995e4d24563ef8510ec521e5b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "209e1f70fc5f6fb3bf16",
+    "url": "/static/js/main.209e1f70.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "209e1f70fc5f6fb3bf16",
+    "url": "/static/css/main.c1404fd8.chunk.css"
+  },
+  {
+    "revision": "1339704329ec8536e7e7df8537a658fc",
+    "url": "/index.html"
+  }
+];
